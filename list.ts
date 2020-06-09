987f906f3e13802d5d3743475ae6497fe24e4702
philipnb <philip@getnabis.com>
2020-06-09 15:03:06 -0700

    added tests for List yay

--- list.ts
+++ list.ts
@@ -1,52 +1,61 @@
-// null is an empty list in this case
-type List<A> = Cons<A> | null
+/**
+ * Functional lists
+ *
+ * some rules:
+ *  1. all functions may only take 0-1 arguments
+ *  2. no loops, only recursion
+ *  3. no side-effects
+ *  4. function is a single return (i break this rule sometimes)
+ *  5. no assignments in functions
+ *  6. no arrays
+ *  7. no ifs
+ *
+ * {@link https://calebharris.github.io/fp_book_club_ts/chapter_3.html}
+ * {@link ./testing/list.test.ts}
+ *
+ */
 
+type Nil = null // the empty list
+type List<A> = Cons<A> | Nil // a list is either the empty or a cons
 type Cons<A> = {
+  // and a cons is a chain of lists
   head: A
   tail: List<A>
 }
+
 const cons = <A>(a: A) => (b: List<A>): List<A> => ({
   head: a,
   tail: b,
 })
+// honestly the errors feel icky
 const head = <A>(xs: List<A>): A => {
   if (!xs) throw new TypeError("Attempted to get head of an empty list")
   else return xs.head
 }
-const tail = <A>(xs: List<A>): List<A> => {
-  if (!xs) throw new TypeError("Attempted to get tail of an empty list")
-  else return xs.tail
-}
+const tail = <A>(xs: List<A>): List<A> => (!xs ? xs : xs.tail)
 
 // return a new list with head, e
 const push = <A>(xs: List<A>) => (e: A) => cons(e)(xs)
 
 // return a new list without the first n elements
 const drop = <A>(xs: List<A>) => (n: BigInt): List<A> =>
-  n <= 0n ? xs : drop(tail(xs))(BigInt(n) - 1n)
+  !xs ? null : n <= 0n ? xs : drop(tail(xs))(BigInt(n) - 1n)
 
 // return a new list with elements in reverse order
 const reverse = <A>(xs: List<A>) => {
-  const loop = (_xs: List<A>) => (accum: List<A>): List<A> => {
-    const [h, t] = [head(xs), tail(xs)]
-    return tail === null ? accum : loop(t)(cons(h)(accum))
-  }
+  const loop = (_xs: List<A>) => (accum: List<A>): List<A> =>
+    _xs === null ? accum : loop(tail(_xs))(cons(head(_xs))(accum))
   return loop(xs)(null)
 }
 
 // return a list with all but the last element
-const init = <A>(xs: List<A>): List<A> => {
-  const [h, t] = [head(xs), tail(xs)]
-  return t === null ? null : cons(h)(init(t))
-}
+const init = <A>(xs: List<A>): List<A> =>
+  !xs ? xs : tail(xs) === null ? null : cons(head(xs))(init(tail(xs)))
 
 // take a list, a start value, and a function. Return a new value
 const foldr = <A>(xs: List<A>) => <B>(z: B) => (
-  f: (elem: A) => (acc: B) => B
-): B => {
-  const [h, t] = [head(xs), tail(xs)]
-  return t === null ? z : foldr(t)(f(h)(z))(f)
-}
+  f: (x: A) => (acc: B) => B
+): B => (xs === null ? z : foldr(tail(xs))(f(head(xs))(z))(f))
 
 // return the length of a list
 const length = <A>(xs: List<A>): number => foldr(xs)(0)((_) => (z) => z + 1)
@@ -54,8 +63,7 @@ const length = <A>(xs: List<A>): number => foldr(xs)(0)((_) => (z) => z + 1)
 // make it easier to display lists in ts
 const listToArray = <A>(xs: List<A>): A[] => {
   const loop = (_xs: List<A>) => (accum: A[]): A[] => {
-    const [h, t] = [head(_xs), tail(_xs)]
-    return t === null ? accum : loop(t)([h, ...accum])
+    return _xs === null ? accum : loop(tail(_xs))([...accum, head(_xs)])
   }
   return loop(xs)([])
 }
@@ -63,11 +71,12 @@ const listToArray = <A>(xs: List<A>): A[] => {
 // make it easier to construct lists in ts
 const arrayToList = <A>(arr: A[]): List<A> => {
   const loop = (_arr: A[]) => (accum: List<A>): List<A> => {
-    return _arr.length === 0
+    const len = _arr.length
+    return len === 0
       ? accum
-      : loop(_arr.slice(1, _arr.length))(cons(_arr[0])(accum))
+      : loop(_arr.slice(0, len - 1))(cons(_arr[len - 1])(accum))
   }
-  return loop(arr)(cons(arr[0])(null))
+  return loop(arr)(null)
 }
 
 export {
