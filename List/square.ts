da24038f03006cfa212d964a8b9ef5b2e18a08bd
philipnb <philip@getnabis.com>
2020-06-13 10:27:57 -0700

    looks like you have to async load wasm to use it

--- List/square.ts
+++ List/square.ts
@@ -1,3 +1,7 @@
 import { List, map } from "./index.ts"
 
 export const square = (xs: List<number>) => map(xs)((x) => x * x)
+
+export const __square__empty = (f: (x: number) => number) => (
+  xs: List<number>
+) => map(xs)(f)
